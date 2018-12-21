7cb8d03f18b7c328a683c38a8bc7789ca5002e8c
Shinya Takahashi <s.takahashi313@gmail.com>
2018-12-22 05:48:44 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# purescript-bucketchain-header-utils
+
+[![Latest release](http://img.shields.io/github/release/Bucketchain/purescript-bucketchain-header-utils.svg)](https://github.com/Bucketchain/purescript-bucketchain-header-utils/releases)
+
+The HTTP header utilities of [Bucketchain](https://github.com/Bucketchain/purescript-bucketchain).
+
+## Installation
+
+```
+bower install purescript-bucketchain-header-utils
+```
+
+## Documentation
+
+Module documentation is [published on Pursuit](http://pursuit.purescript.org/packages/purescript-bucketchain-header-utils).
+
+## LICENSE
+
+MIT
---
+++ src/Bucketchain/Header/Cookie.purs
@@ -0,0 +1,91 @@
+module Bucketchain.Header.Cookie
+  ( Cookie
+  , addCookie
+  , getCookies
+  ) where
+
+import Prelude
+
+import Bucketchain.Http (Http, setHeaders, requestHeaders, responseHeaders)
+import Data.Array (snoc, (!!))
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.String (Pattern(..), split, trim, joinWith)
+import Data.Tuple (Tuple(..))
+import Effect (Effect)
+import Foreign.Object (Object, lookup, empty, fromFoldable)
+
+-- | The cookie payload.
+type Cookie =
+  { key :: String
+  , value :: String
+  , domain :: Maybe String
+  , path :: Maybe String
+  , maxAge :: Maybe Int
+  , secure :: Boolean
+  , httpOnly :: Boolean
+  }
+
+-- | Add a cookie.
+addCookie :: Http -> Cookie -> Effect Unit
+addCookie http pld =
+  setHeaders http "Set-Cookie" $ snoc resCookies $ toField pld
+  where
+    resCookies = responseHeaders http "set-cookie"
+
+toField :: Cookie -> String
+toField pld =
+  joinWith "; "
+    <<< setHttpOnly pld
+    <<< setSecure pld
+    <<< setMaxAge pld
+    <<< setPath pld
+    <<< setDomain pld
+    $ [ pld.key <> "=" <> pld.value ]
+
+setDomain :: Cookie -> Array String -> Array String
+setDomain pld xs =
+  case pld.domain of
+    Nothing -> xs
+    Just d -> snoc xs $ "Domain=" <> d
+
+setPath :: Cookie -> Array String -> Array String
+setPath pld xs =
+  case pld.path of
+    Nothing -> xs
+    Just p -> snoc xs $ "Path=" <> p
+
+setMaxAge :: Cookie -> Array String -> Array String
+setMaxAge pld xs =
+  case pld.maxAge of
+    Nothing -> xs
+    Just i -> snoc xs $ "Max-Age=" <> show i
+
+setSecure :: Cookie -> Array String -> Array String
+setSecure pld xs =
+  if pld.secure
+    then snoc xs "Secure"
+    else xs
+
+setHttpOnly :: Cookie -> Array String -> Array String
+setHttpOnly pld xs =
+  if pld.httpOnly
+    then snoc xs "HttpOnly"
+    else xs
+
+-- | Get cookies.
+getCookies :: Http -> Object String
+getCookies http =
+  case getCookieStr http of
+    Nothing -> empty
+    Just str ->
+      fromFoldable $ map (trim >>> toTuple) $ split (Pattern ";") str
+
+getCookieStr :: Http -> Maybe String
+getCookieStr = requestHeaders >>> lookup "cookie"
+
+toTuple :: String -> Tuple String String
+toTuple str =
+  let ary = split (Pattern "=") str
+      key = fromMaybe "" $ ary !! 0
+      val = fromMaybe "" $ ary !! 1
+   in Tuple key val
